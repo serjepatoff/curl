83197ec64085a6d0e11beebd876b073b4a4283f0
Sergei Epatov <sepatov@readdle.com>
2015-07-01 14:18:29 +0300

    - iOS 8.0+ framework project

---
+++ projects/iOS/libcurlFramework/libcurl/exports.h
@@ -0,0 +1,17 @@
+//
+//  exports.h
+//  libcurl
+//
+//  Created by Sergei Epatov on 7/1/15.
+//  Copyright (c) 2015 Readdle. All rights reserved.
+//
+
+//! Project version number for libcurl.
+FOUNDATION_EXPORT double libcurlVersionNumber;
+
+//! Project version string for libcurl.
+FOUNDATION_EXPORT const unsigned char libcurlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libcurl/PublicHeader.h>
+
+
